f8810f03c32fcefc06503547d1b6df6996c4e5b2
Rui Santos <ee11077@fe.up.pt>
2016-04-29 11:32:17 +0100

    Create mqtt_esp8266_sockets.ino

---
+++ code/mqtt_esp8266_sockets.ino
@@ -0,0 +1,135 @@
+/*****
+ 
+ All the resources for this project:
+ https://rntlab.com/
+ 
+*****/
+
+// Loading the ESP8266WiFi library and the PubSubClient library
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+// Loading the RCSwitch library to control the outlets
+#include <RCSwitch.h>
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "YOUR_RPi_IP_Address";
+
+// Initializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Initializes the RCSwitch
+RCSwitch mySwitch = RCSwitch();
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic home/office/esp1/gpio2, you check if the message is either 1 or 0. Turns the ESP GPIO according to the message
+  if(topic=="home/office/esp1/desk"){
+      Serial.print("Changing Desk Light to ");
+      if(messageTemp == "1"){
+        // Sends binary code to turn on Desk Light
+        mySwitch.send("000101010101000101010101");
+        Serial.print("On");
+      }
+      else if(messageTemp == "0"){
+        // Sends binary code to turn off Desk Light
+        mySwitch.send("000101010101000101010100");
+        Serial.print("Off");
+      }
+  }
+  if(topic=="home/office/esp1/workbench"){
+      Serial.print("Changing Workbench Light to ");
+      if(messageTemp == "1"){
+        // Sends binary code to turn on Workbench Light
+        mySwitch.send("000101010101010001010101");
+        Serial.print("On");
+      }
+      else if(messageTemp == "0"){
+        // Sends binary code to turn off Workbench Light
+        mySwitch.send("000101010101010001010100");
+        Serial.print("Off");
+      }
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe("home/office/esp1/desk");
+      client.subscribe("home/office/esp1/workbench");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  mySwitch.enableTransmit(16);
+}
+
+// For this project, you don't need to change anything in the loop function. Basically it ensures that you ESP is connected to your broker
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
