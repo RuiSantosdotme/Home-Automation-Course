14d3b2fa4c5778a537e03f9dfa2425ab7b1079d4
Rui Santos <ee11077@fe.up.pt>
2016-11-21 23:14:51 +0000

    Create sonoff_with_node_red.ino

---
+++ code/sonoff_with_node_red.ino
@@ -0,0 +1,137 @@
+/*****
+ 
+ All the resources for this project:
+ https://rntlab.com/
+ 
+*****/
+
+// Loading the ESP8266WiFi library and the PubSubClient library
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "YOUR_RPi_IP_Address";
+
+// Initializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// GPIOs of your ESP8266 on your SONOFF
+int gpio13Led = 13;
+int gpio12Relay = 12;
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic home/office/sonoff1, you check if the message is either 1 or 0. Turns the ESP GPIO according to the message
+  if(topic=="home/office/sonoff1"){
+      Serial.print("Changing Sonoff to ");
+      if(messageTemp == "1"){
+        digitalWrite(gpio13Led, LOW);
+        digitalWrite(gpio12Relay, HIGH);
+        Serial.print("On");
+      }
+      else if(messageTemp == "0"){
+        digitalWrite(gpio13Led, HIGH);
+        digitalWrite(gpio12Relay, LOW);
+        Serial.print("Off");
+      }
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    /*
+     YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+     To change the ESP device ID, you will have to give a new name to the ESP8266.
+     Here's how it looks:
+       if (client.connect("ESP8266Client")) {
+     You can do it like this:
+       if (client.connect("ESP1_Office")) {
+     Then, for the other ESP:
+       if (client.connect("ESP2_Garage")) {
+      That should solve your MQTT multiple connections problem
+    */
+    if (client.connect("sonoff1")) {
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more Sonoffs)
+      client.subscribe("home/office/sonoff1");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+  // preparing GPIOs
+  pinMode(gpio13Led, OUTPUT);
+  digitalWrite(gpio13Led, HIGH);
+  
+  pinMode(gpio12Relay, OUTPUT);
+  digitalWrite(gpio12Relay, HIGH);
+  
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+// For this project, you don't need to change anything in the loop function. 
+// Basically it ensures that you ESP is connected to your broker
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+    client.connect("sonoff1");
+}
