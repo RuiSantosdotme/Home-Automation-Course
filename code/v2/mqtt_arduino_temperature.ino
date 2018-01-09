81eb673c76335c92a4260646f75d68fb898ed04b
Rui Santos <ee11077@fe.up.pt>
2018-01-09 11:10:05 +0000

    set protocol

--- code/v2/mqtt_arduino_temperature.ino
+++ code/v2/mqtt_arduino_temperature.ino
@@ -113,11 +113,12 @@ void reconnect() {
 void setup()
 {
   mySwitch.enableTransmit(6);
-  // SET YOUR PULSE LENGTH
-  mySwitch.setPulseLength(REPLACE_WITH_YOUR_PULSE_LENGTH);
-  
+ 
   // SET YOUR PROTOCOL (default is 1, will work for most outlets)
   mySwitch.setProtocol(REPLACE_WITH_YOUR_PROTOCOL);
+
+   // SET YOUR PULSE LENGTH
+  mySwitch.setPulseLength(REPLACE_WITH_YOUR_PULSE_LENGTH);
   
   // Set number of transmission repetitions.
   mySwitch.setRepeatTransmit(15);
